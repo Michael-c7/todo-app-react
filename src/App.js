00317a91d9bbcf29879dd151f1e39dbad91d491b
Michael Carr <mcarr2468@gmail.com>
2021-09-16 19:16:26 -0500

    V1.0 finished html & css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+import { FaEdit, FaTrash } from 'react-icons/fa'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="todo">
+      <h1 className="todo-header">Todo app</h1>
+
+      <form className="todo__form">
+        <label htmlFor="item"></label>
+        <input className="form__input" type="text" name="item" id="item" maxlength="40" placeholder="Eg: Bread" autoFocus={true}/>
+        <button className="form__submit-btn" type="submit">Submit</button>
+      </form>
+
+
+      <ul className="todo__items">
+        <li className="todo__item">
+          <div className="todo__item__text">cake</div>
+          <div className="todo__item__btns">
+            <button className="item__btns__edit-btn" type="button"><FaEdit/></button>
+            <button className="item__btns__delete-btn" type="button"><FaTrash/></button>
+          </div>
+        </li>
+
+        <li className="todo__item">
+          <div className="todo__item__text">milk</div>
+          <div className="todo__item__btns">
+            <button className="item__btns__edit-btn" type="button"><FaEdit/></button>
+            <button className="item__btns__delete-btn" type="button"><FaTrash/></button>
+          </div>
+        </li>
+
+        <li className="todo__item">
+          <div className="todo__item__text">eggs</div>
+          <div className="todo__item__btns">
+            <button className="item__btns__edit-btn" type="button"><FaEdit/></button>
+            <button className="item__btns__delete-btn" type="button"><FaTrash/></button>
+          </div>
+        </li>
+      </ul>
+
+      <button className="delete-all-btn" type="button">Clear Items</button>
+    </section>
+
+
+  )
 }
 
-export default App;
+export default App
